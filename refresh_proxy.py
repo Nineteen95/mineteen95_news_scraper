1f771fb90467de486439cdeabe82f24301194d9e
andreyinozemcev <as.inozemtsev@outlook.com>
2023-03-17 20:05:41 +0300

    Исправлен код в модуле Теперь функция правильно добавляет новые прокси в список и отлавливает исключения при обновлении списка.

--- refresh_proxy.py
+++ refresh_proxy.py
@@ -1,15 +1,23 @@
-async def start_proxy_finder():
+import asyncio
+from datetime import datetime, timedelta
+from typing import List
+
+from proxy import Proxy
+
+
+async def refresh_proxy(find_proxies_func, proxies: List[Proxy], refresh_interval: int):
+    """
+    Асинхронная функция, которая обновляет список прокси-серверов каждые refresh_interval секунд
+    :param find_proxies_func: функция для поиска новых прокси
+    :param proxies: список текущих прокси
+    :param refresh_interval: интервал обновления списка прокси в секундах
+    """
     while True:
-        await find_proxies()
-        time.sleep(60)  # ждать 60 секунд перед следующей итерацией
-
-if __name__ == "__main__":
-    asyncio.run(start_proxy_finder())
-
-import threading
-from proxy import find_proxies
-
-def start_proxy_finder():
-    t = threading.Thread(target=find_proxies, args=())
-    t.daemon = True
-    t.start()
+        try:
+            new_proxies = await find_proxies_func()
+            for p in new_proxies:
+                if p not in proxies:
+                    proxies.append(p)
+            await asyncio.sleep(refresh_interval)
+        except Exception as e:
+            print(f"Error refreshing proxies: {e}")
